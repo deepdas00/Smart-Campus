872ee5bc6cec5633341df0d8e30a582ea0cd3ba2
Deep Das <itsdeep890@gmail.com>
2025-12-18 11:04:19 +0530

    adding profile page just the structure to build further details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Page/HomePage";
 import SignupPage from "./Page/SignupPage"; // create this
+import Profile from "./Page/Profile";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<SignupPage />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }

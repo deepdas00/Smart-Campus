1ecf87d32951bbfec33fa0829694271a64c6de40
sangita-ghosh <01sangita.ghosh@gmail.com>
2025-12-19 11:22:50 +0530

    testing

--- frontend/src/Page/Profile.jsx
+++ frontend/src/Page/Profile.jsx
@@ -2,10 +2,44 @@ import React from 'react'
 
 function Profile() {
   return (
+    <>
     <div>
       hello
       dev
+      hello
+      dev
+      hello
+      dev
+      hello
+      dev
+      hello
+      dev
     </div>
+    <div>
+      hello
+      dev
+      hello
+      dev
+      hello
+      dev
+      hello
+      dev
+      hello
+      dev
+    </div>
+    <div>
+      hello
+      dev
+      hello
+      dev
+      hello
+      dev
+      hello
+      dev
+      hello
+      dev
+    </div>
+    </>
   )
 }
 

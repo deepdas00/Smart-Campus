e9b8c4541e2df89470bf7ba3579e4d7dd31c5620
Deep Das <itsdeep890@gmail.com>
2026-01-10 16:25:30 +0530

    SendMail

--- backend/src/utils/sendMail.js
+++ backend/src/utils/sendMail.js
@@ -2,29 +2,35 @@ import nodemailer from "nodemailer";
 
 export const sendMail = async ({ to, subject, html }) => {
   console.log("SMTP TRYING TO CONNECT");
-  
+
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    host: "smtp.gmail.com",
+    port: 587,
     secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
     tls: {
       rejectUnauthorized: false,
-      minVersion: "TLSv1.2"
+      minVersion: "TLSv1.2",
+      servername: "smtp.gmail.com"
     }
   });
 
-  
+  await transporter.verify();
+
+  console.log("SMTP CONNECTED");
 
   await transporter.sendMail({
     from: `"Smart Campus System" <${process.env.SMTP_USER}>`,
     to,
     subject,
-    html,
+    html
   });
 
-
+  console.log("MAIL SENT");
 };

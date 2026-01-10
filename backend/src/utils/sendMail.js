aa25370ea7f97039bf87cb208821359c20baec8b
Deep Das <itsdeep890@gmail.com>
2026-01-10 17:00:21 +0530

    Done

--- backend/src/utils/sendMail.js
+++ backend/src/utils/sendMail.js
@@ -1,36 +1,59 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
+
+// export const sendMail = async ({ to, subject, html }) => {
+//   console.log("SMTP TRYING TO CONNECT");
+
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//       user: process.env.SMTP_USER,
+//       pass: process.env.SMTP_PASS,
+//     },
+//     connectionTimeout: 10000,
+//     greetingTimeout: 10000,
+//     socketTimeout: 10000,
+//     tls: {
+//       rejectUnauthorized: false,
+//       minVersion: "TLSv1.2",
+//       servername: "smtp.gmail.com"
+//     }
+//   });
+
+//   await transporter.verify();
+
+//   console.log("SMTP CONNECTED");
+
+//   await transporter.sendMail({
+//     from: `"Smart Campus System" <${process.env.SMTP_USER}>`,
+//     to,
+//     subject,
+//     html
+//   });
+
+//   console.log("MAIL SENT");
+// };
+
+
+
+
+
+
+
+import SibApiV3Sdk from "sib-api-v3-sdk";
 
 export const sendMail = async ({ to, subject, html }) => {
-  console.log("SMTP TRYING TO CONNECT");
+  const client = SibApiV3Sdk.ApiClient.instance;
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-    tls: {
-      rejectUnauthorized: false,
-      minVersion: "TLSv1.2",
-      servername: "smtp.gmail.com"
-    }
-  });
+  client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
 
-  await transporter.verify();
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-  console.log("SMTP CONNECTED");
-
-  await transporter.sendMail({
-    from: `"Smart Campus System" <${process.env.SMTP_USER}>`,
-    to,
+  await apiInstance.sendTransacEmail({
+    sender: { email: "smartcampus.official@gmail.com", name: "Smart Campus" },
+    to: [{ email: to }],
     subject,
-    html
+    htmlContent: html
   });
-
-  console.log("MAIL SENT");
 };

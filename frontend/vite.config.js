abec4d38542dd00396d696363f4499234dc247fe
Deep Das <itsdeep890@gmail.com>
2026-01-11 10:04:36 +0530

    TOUCH done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { qrcode } from "vite-plugin-qrcode";
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), qrcode()],
@@ -16,14 +15,24 @@ export default defineConfig({
       "spin-slow": "spin 3s linear infinite",
     },
     keyframes: {
-        shimmer: {
-          '100%': { transform: 'translateX(100%)' },
-        },
+      shimmer: {
+        "100%": { transform: "translateX(100%)" },
       },
+      wave: {
+        "0%": { transform: "translateX(-100%)" },
+        "100%": { transform: "translateX(300%)" },
+      },
+       page: {
+      "0%": { transform: "rotateY(0deg)" },
+      "100%": { transform: "rotateY(-180deg)" },
+    },
+    },
+    animation: {
+    wave: "wave 1.3s ease-in-out infinite",
+    page: "page 1s ease-in-out infinite",
+  },
   },
 });
-
-
 
 // export default defineConfig({
 //   plugins: [react(), tailwindcss()],
